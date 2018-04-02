c4545abb7719c3e611ae906c1363a0f3d069d97e
Eric Perez <ebperez@unomaha.edu>
2018-04-02 14:30:55 -0500

    Initial Commit

---
+++ prog2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prog2
+//
+//  Created by Eric Perez on 4/2/18.
+//  Copyright © 2018 Eric Perez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
